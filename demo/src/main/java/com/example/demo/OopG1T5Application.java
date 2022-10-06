62a668d5551494c70f41449b66e816a3f36fde7d
valeriewoon <valeriewoon.2020@scis.smu.edu.sg>
2022-10-06 22:31:25 +0800

    initialised springboot project

---
+++ demo/src/main/java/com/example/demo/OopG1T5Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopG1T5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OopG1T5Application.class, args);
+	}
+
+}
