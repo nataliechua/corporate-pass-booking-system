6bda7168c50647dfaf380cc6f63393ad4f7d66fa
valeriewoon <valeriewoon.2020@scis.smu.edu.sg>
2022-10-07 17:43:57 +0800

    new intialization java project

---
+++ demo/src/main/java/Is424G1T5Application.java
@@ -0,0 +1,13 @@
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Is424G1T5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Is424G1T5Application.class, args);
+	}
+
+}
