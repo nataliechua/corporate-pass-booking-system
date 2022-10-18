7f646a911f20d310b2cc30f8cc5d6ce86e29b812
Lin Peishan <linpeishann@gmail.com>
2022-10-18 13:29:26 +0800

    Customized login page
    modified frontend directory
    added SecurityConfig file
    added homecontroller

---
+++ demo/src/main/java/project/SecurityConfig.java
@@ -0,0 +1,30 @@
+package project;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter { // replace default login page w our customised ver
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        String[] staticResources  =  {
+            "/css/**",
+            "/vendor/fontawesome-free/css/**",
+            "/vendor/jquery/**",
+            "/vendor/bootstrap/js/**",
+            "/vendor/jquery-easing/**",
+            "/js/**",
+        };
+        http
+            .authorizeRequests()
+            .antMatchers(staticResources).permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .formLogin()
+            .loginPage("/login") // use a customised login page
+            .permitAll();
+    }
+    
+}
