9b7873fac6cb2156ea46fc74146d7f942eec5b78
Lin Peishan <linpeishann@gmail.com>
2022-10-18 19:23:57 +0800

    Added routes to html pages

--- demo/src/main/java/project/controller/HomeController.java
+++ demo/src/main/java/project/controller/HomeController.java
@@ -1,21 +1,60 @@
 package project.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import project.service.StaffService;
+import project.entity.*;
+import project.service.*;
+
+import java.util.*;
+
 @Controller
 public class HomeController {
 
+    @Autowired
+    private StaffService staffService;
+
     @GetMapping("/")
-    String index() {
+    public String index() {
         return "index";
     }
 
     @GetMapping("/login")
     public String welcome() {
         return "login";
+    }
+
+    @RequestMapping("/viewStaffs") 
+    public String viewStaffs(Model model) {
+
+        List<Staff> staffs = staffService.getAllStaff(); 
+        model.addAttribute("staffs", staffs);
+        return "staffs";
+    }
+
+    @GetMapping("/viewPasses")
+    public String viewPasses() {
+        return "passes";
+    }
+
+    @GetMapping("/gopReturnPass")
+    public String gopReturnPass() {
+        return "gopReturnPass";
+    }
+
+    @GetMapping("/bookAPass")
+    public String bookAPass() {
+        return "bookAPass";
+    }
+
+    @GetMapping("/loanedPasses")
+    public String loanedPasses() {
+        return "loanedPasses";
     }
 }
