b2960715657448d7dd7dd3d555223736c39866da
valeriewoon <valeriewoon.2020@scis.smu.edu.sg>
2022-10-31 22:20:33 +0800

    send email function, not integrated with velocity nor frontend

--- demo/src/main/java/project/Is424G1T5Application.java
+++ demo/src/main/java/project/Is424G1T5Application.java
@@ -1,5 +1,12 @@
 package project;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+import javax.mail.MessagingException;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,8 +19,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Is424G1T5Application {
 
+	// to send email upon app running, need to have frontend to send email first to link this functionality there
+	// @Autowired
+	// private EmailSenderService service;
 	public static void main(String[] args) {
 		SpringApplication.run(Is424G1T5Application.class, args);
+	
 	}
+
+	// uncommment below to send email, temporarily put here till frontend has send email button
+	// @EventListener(ApplicationReadyEvent.class)
+	// public void triggerMail(){
+	// 	service.sendSimpleEmail("testemailjava91@gmail.com", "email body", "email test");
+	// }
 
 }
