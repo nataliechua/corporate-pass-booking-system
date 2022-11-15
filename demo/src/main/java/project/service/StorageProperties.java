2adf0e892bc462608a09d52084a26e1d348bb293
kaijun199 <55582844+kaijun199@users.noreply.github.com>
2022-11-16 00:03:45 +0800

    UP navigation + upload txt file

--- demo/src/main/java/project/service/StorageProperties.java
+++ demo/src/main/java/project/service/StorageProperties.java
@@ -5,7 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties("storage")
 public class StorageProperties {
 
-	private String location = "/wamp64\\www\\OOP\\demo\\src\\main\\resources\\templates\\emailTemplates";
+	private String location = "/wamp64\\www\\group-project-g1t5\\demo\\src\\main\\resources\\templates\\emailTemplates";
 
 	public String getLocation() {
 		return location;
